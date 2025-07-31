fa88b7d77657c9edeb164106d4b04e9552e7e410
kush bhardwaj <kush@qurilo.com>
2025-07-31 13:11:45 +0530

    project setup compelete with server

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config({})
+const app = require('./app');
+const http = require('http');
+const server = http.createServer(app);
+
+const Port = process.env.PORT || 3000;
+
+server.listen(Port, () => {
+    console.log(`Server is running on port ${Port}`);
+    });
